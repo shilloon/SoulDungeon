d7056882802d0d92031545a36d27daa2ed49c446
shilloon <only1471@gmail.com>
2026-01-22 23:42:17 +0900

    2. Add TempCharacter asset
    1. Add Characterbase c++ class
    2. Add PlayerCharacter test asset

---
+++ Source/SoulDungeon/Private/Character/SoulCharacterBase.cpp
@@ -0,0 +1,31 @@
+#include "Character/SoulCharacterBase.h"
+
+// Sets default values
+ASoulCharacterBase::ASoulCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASoulCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASoulCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASoulCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
